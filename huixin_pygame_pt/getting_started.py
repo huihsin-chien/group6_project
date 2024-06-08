969939e3e7706877fc2d579e4046d956824f202d
jianhuixin <comet112511092.ee12@nycu.edu.tw>
2024-06-08 17:58:12 +0800

    i'm new in pygame

--- huixin_pygame_pt/getting_started.py
+++ huixin_pygame_pt/getting_started.py
@@ -11,15 +11,32 @@ canvas = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Show image!")
 
 image = pygame.image.load(".\OneDrive\git_repo\group6_project\huixin_pygame_pt\image.JPG")
+optimized_image =image.convert()
+optimized_image.set_colorkey((255,255,255))
+
+clock = pygame.time.Clock()
+clock.tick(60)
+
 exit = False
+
+show_image = True
 
 while not exit:
     canvas.fill(color)
-    canvas.blit(image, dest = position)
+    # canvas.blit(optimized_image, dest = position)
 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
-    pygame.draw.rect(canvas, rect_color, pygame.Rect(30,30,60,60))
+    if show_image:
+        canvas.blit(optimized_image, dest = position)
+        show_image = False
+    else:
+        pygame.draw.rect(canvas, rect_color, [30,30,60,60])
+        pygame.draw.circle(canvas, rect_color,[400,400], 50,0)
+        pygame.draw.polygon(canvas, 'blue', [[0,0],[50,50],[250,250],[150,20]])
+        pygame.draw.line(canvas, 'blue', [0,0], [400,400], 5)
+        show_image = True
     pygame.display.update()
+    pygame.time.delay(1000)
