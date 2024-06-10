7016955cdc640fdc86877a8e66c90317bb5d8ed1
jianhuixin <comet112511092.ee12@nycu.edu.tw>
2024-06-11 02:48:52 +0800

    more new buttons

--- Final_pj/screens/game_screen.py
+++ Final_pj/screens/game_screen.py
@@ -43,8 +43,8 @@ def draw_player_info(screen, shop_button):
 def game_screen(screen):
 
     shop_button = Button(u'Shop', (500, 150), menu_font,screen, GRAY, u'Shop')
-    food_button = Button(u'Food', (50, 200), menu_font,screen, GRAY, u'Food')
-    water_button = Button(u'Water', (50, 250), menu_font,screen, GRAY, u'Water')
+    food_button = Button(f'Food:{pet.food_amount}', (50, 200), menu_font,screen, GRAY, f'Food{pet.food_amount}')
+    water_button = Button(f'Water:{pet.water_amount}', (50, 250), menu_font,screen, GRAY, f'Water:{pet.water_amount}')
 
     while True:
         for event in pygame.event.get():
@@ -54,11 +54,14 @@ def game_screen(screen):
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if shop_button.click(event):
-                    open_shop()
+                    open_shop(screen)
                 if food_button.click(event):
                     pet.feed()
+                    food_button = Button(f'Food:{pet.food_amount}', (50, 200), menu_font,screen, GRAY, f'Food{pet.food_amount}')
                 if water_button.click(event):
                     pet.drink()
+                    water_button = Button(f'Water:{pet.water_amount}', (50, 250), menu_font,screen, GRAY, f'Water:{pet.water_amount}')
+
             elif event.type == pygame.MOUSEBUTTONUP:
                 shop_button.release(event)
                 food_button.release(event)
@@ -70,13 +73,35 @@ def game_screen(screen):
         pet.update_hour()
         pygame.display.update()
 
-def open_shop():
-    screen = pygame.display.get_surface()
+def open_shop(screen):
+    # screen = pygame.display.get_surface()
+    buy_food_button = Button(u'Food: 3 dollar', (300, 200), menu_font, screen, GRAY, u'Food: 3 dollar')
+    buy_water_button = Button(u'Water: 1 dollar', (300, 300), menu_font, screen, GRAY, u'Water: 1 dollar')
+    return_to_game_button = Button(u'Return to Game', (300, 400), menu_font, screen, GRAY, u'Return to Game')
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if buy_food_button.click(event):
+                    pet.buy_food()
+                if buy_water_button.click(event):
+                    pet.buy_water()
+                if return_to_game_button.click(event):
+                    game_screen(screen)
+            elif event.type == pygame.MOUSEBUTTONUP:
+                buy_food_button.release(event)
+                buy_water_button.release(event)
         screen.fill(BLACK)
+        buy_food_button.show()
+        buy_water_button.show()
+        return_to_game_button.show()
+        ### draw_pet_attributes: food amount, water amount, money
+        money_text = menu_font.render(f'Money: {pet.money}', True, WHITE)
+        screen.blit(money_text, (500, 100))
+        food_text = menu_font.render(f'Food: {pet.food_amount}', True, WHITE)
+        screen.blit(food_text, (500, 150))
+        water_text = menu_font.render(f'Water: {pet.water_amount}', True, WHITE)
+        screen.blit(water_text, (500, 200))
         pygame.display.update()
