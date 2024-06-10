e986b3c936a24539b41e1580399c29cf46f86789
jianhuixin <comet112511092.ee12@nycu.edu.tw>
2024-06-10 18:37:23 +0800

    rel path

--- Final_pj/settings.py
+++ Final_pj/settings.py
@@ -1,4 +1,5 @@
 import pygame
-menu_background_image_path = "OneDrive\git_repo\group6_project\Final_pj\img\menu_background.jpg"
-font_path = "OneDrive\git_repo\group6_project\Final_pj\_font\HanyiSentyPagoda Regular.ttf"
+pygame.font.init()
+menu_background_image_path = "OneDrive\git_repo\group6_project\Final_pj\Assets\img\menu_background.jpg"
+font_path = "OneDrive\git_repo\group6_project\Final_pj\Assets\_font\HanyiSentyPagoda Regular.ttf"
 menu_font_size = 36
